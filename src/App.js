f223b215217903ff9d72436130357b94880932e1
shykulamykhailo <lucamike332@gmail.com>
2024-01-21 22:08:47 +0200

    1

--- src/App.js
+++ src/App.js
@@ -177,6 +177,7 @@ function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating))
   const avgUserRating = average(watched.map((movie) => movie.userRating))
   const avgRuntime = average(watched.map((movie) => movie.runtime))
+  console.log(avgRuntime)
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
