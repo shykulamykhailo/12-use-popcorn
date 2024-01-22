3ee1fdd00a264a0335188cf657f4daf551bdcf49
shykulamykhailo <lucamike332@gmail.com>
2024-01-22 21:10:42 +0200

    1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ import StarRating from './components/StarRating'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['terrible', 'bad', 'ok', 'good', 'amazing']}
+    />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 )
